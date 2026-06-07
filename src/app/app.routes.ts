bef549dec1697e8af390403e3dfb02afa7fd6063
BrayanT99 <brayan201115@gmail.com>
2026-06-06 20:50:40 -0500

    feat: configurar HttpClient, animaciones y rutas de la aplicacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { FacturaComponent } from './components/factura/factura.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: FacturaComponent }
+];
