e8e105b71d7a8dcbab156b61b7ccbfa1e86f98f6
BrayanT99 <brayan201115@gmail.com>
2026-06-06 21:02:21 -0500

    chore: configurar AppComponent para renderizar FacturaComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FacturaComponent } from './components/factura/factura.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FacturaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
